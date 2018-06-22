9a5efeac11d1fb51593a5342f0702a08d98ff959
Hang Cui <oscarcuihang@hotmail.com>
2018-06-21 22:27:00 -0500

    Add Models and controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: 'chatrooms#index'
-  resources :chatrooms
+
+  resources :chatrooms do
+    resource :chatroom_users
+    resources :messages
+  end
+
   devise_for :users
 end
